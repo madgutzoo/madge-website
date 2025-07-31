c939cb07d99a6ab6a05cb864d1c3af031a07a2d6
Joel Kram <joelnskram@gmail.com>
2025-07-31 19:38:39 +0100

    init: setup react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Header } from './Header';
+import { MainPage } from './MainPage';
+import { Footer } from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainPage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="">
+      <h1>MADGE JINGCHU ZHOU</h1>
+    </div>
+  );
+};
