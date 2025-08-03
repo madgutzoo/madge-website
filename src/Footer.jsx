94966289732fe7321985730f227c668e7dbbd791
Joel Kram <joelnskram@gmail.com>
2025-08-03 14:06:23 +0100

    updated: font size and tracking

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full p-4 z-50 text-6xl">
+    <div className="fixed bottom-0 w-full p-4 z-50 text-7xl tracking-widest">
       <h2>UNDER CONSTRUCTION</h2>
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="font-monument text-6xl p-4 bg-blue-300/45 w-3/4 tracking-wide">
+    <div className="font-monument text-8xl p-4 bg-blue-300/45 w-full tracking-widest">
       <h1>MADGE JINGCHU ZHOU</h1>
     </div>
   );
