ab436ff9bb492300ada662e09c27588d3f727180
Joel Kram <joelnskram@gmail.com>
2025-08-03 20:32:02 +0100

    updated: instagram url

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,7 +3,7 @@ export const MainPage = () => {
     <div className="ml-2 text-2xl flex flex-row items-center gap-4 mt-4  text-white">
       <p>madgutzoo@gmail.com</p>
 
-      <a href="https://www.instagram.com/moumoumouuuuu/">
+      <a href="https://www.instagram.com/madgutzoo/">
         <img
           src="/instagram-svgrepo-com.svg"
           alt="instagram icon, click here to go to the Instagram page"
